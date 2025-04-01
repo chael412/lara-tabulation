a18ad24ed42b9d788c59867ee128c27f79701233
chael412 <165856017+chael412@users.noreply.github.com>
2025-04-01 23:07:27 +0800

    test url

--- resources/js/hooks/useAppUrl.jsx
+++ resources/js/hooks/useAppUrl.jsx
@@ -1,9 +1,9 @@
 import { useMemo } from "react";
 
 const useAppUrl = () => {
-    // Define the URL constants for offline and online
-    const appUrl = "http://localhost:8000";
-    //const appUrl = "https://piton.chaelx.online";
+    // Define the URL constants for offline and online test
+    //const appUrl = "http://localhost:8000";
+    const appUrl = "https://piton.chaelx.online";
 
     const API_URL = useMemo(() => {
         return appUrl;
